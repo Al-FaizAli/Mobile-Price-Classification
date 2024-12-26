103601f5847f2e12800a084ad2b3b3e7e61091ab
Al Faiz Ali <alfaizali8410@gmail.com>
2024-12-27 04:06:13 +0530

    main.py modified

--- main.py
+++ main.py
@@ -46,34 +46,34 @@ def main():
 
     # the following lines create text boxes in which the user can enter
     # the data required to make the prediction
-    battery_power= st.text_input("battery_power", "Type Here")
-    blue = st.text_input("blue", "Type Here")
-    clock_speed = st.text_input("clock_speed", "Type Here")
-    dual_sim = st.text_input("dual_sim", "Type Here")
-    fc = st.text_input("fc", "Type Here")
-    four_g = st.text_input("four_g", "Type Here")
-    int_memory = st.text_input("int_memory", "Type Here")
-    m_dep = st.text_input("m_dep", "Type Here")
-    mobile_wt = st.text_input("mobile_wt", "Type Here")
-    n_cores = st.text_input("n_cores", "Type Here")
-    pc = st.text_input("pc", "Type Here")
-    px_height = st.text_input("px_height", "Type Here")
-    px_width = st.text_input("px_width", "Type Here")
-    ram = st.text_input("ram", "Type Here")
-    sc_h = st.text_input("sc_h", "Type Here")
-    sc_w = st.text_input("sc_w", "Type Here")
-    talk_time = st.text_input("talk_time", "Type Here")
-    three_g = st.text_input("three_g", "Type Here")
-    touch_screen = st.text_input("touch_screen", "Type Here")
-    wifi = st.text_input("wifi", "Type Here")
+    battery_power = st.number_input("Battery Power", value=0)
+    blue = st.number_input("Bluetooth (0 or 1)", value=0)
+    clock_speed = st.number_input("Clock Speed", value=0.0)
+    dual_sim = st.number_input("Dual SIM (0 or 1)", value=0)
+    fc = st.number_input("Front Camera MP", value=0)
+    four_g = st.number_input("4G (0 or 1)", value=0)
+    int_memory = st.number_input("Internal Memory (GB)", value=0)
+    m_dep = st.number_input("Mobile Depth (cm)", value=0.0)
+    mobile_wt = st.number_input("Mobile Weight (grams)", value=0)
+    n_cores = st.number_input("Number of Cores", value=0)
+    pc = st.number_input("Primary Camera MP", value=0)
+    px_height = st.number_input("Pixel Height", value=0)
+    px_width = st.number_input("Pixel Width", value=0)
+    ram = st.number_input("RAM (MB)", value=0)
+    sc_h = st.number_input("Screen Height", value=0)
+    sc_w = st.number_input("Screen Width", value=0)
+    talk_time = st.number_input("Talk Time (hours)", value=0)
+    three_g = st.number_input("3G (0 or 1)", value=0)
+    touch_screen = st.number_input("Touch Screen (0 or 1)", value=0)
+    wifi = st.number_input("WiFi (0 or 1)", value=0)
     result = ""
 
     # the below line ensures that when the button called 'Predict' is clicked,
     # the prediction function defined above is called to make the prediction
     # and store it in the variable result
     if st.button("Predict"):
-        result = prediction(battery_power,blue,clock_speed,dual_sim,fc,four_g,int_memory,m_dep,mobile_wt,n_cores,pc,px_height,px_width,ram,sc_h,sc_w,talk_time,three_g,touch_screen,wifi)
-    st.success('The output is {}'.format(result))
+        result = prediction(battery_power, blue, clock_speed, dual_sim, fc, four_g, int_memory, m_dep, mobile_wt, n_cores, pc, px_height, px_width, ram, sc_h, sc_w, talk_time, three_g, touch_screen, wifi)
+        st.success('The output is {}'.format(result))
 
 if __name__ == '__main__':
     main()
